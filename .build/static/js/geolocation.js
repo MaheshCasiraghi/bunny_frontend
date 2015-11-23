5bfa3a2cf6f9c03d7cc7980c564c6a787cd2a974
Mahesh Casiraghi <mahesh@coffeestrap.com>
2015-11-23 16:58:38 -0500

    Script Loading Synch Fixed and Twitter Layout Improved.

--- .build/static/js/geolocation.js
+++ .build/static/js/geolocation.js
@@ -1,24 +1,42 @@
 
+// Wait for the geocoder object to be instatntiated before 
+// looking up for the position object through the navigator 
+// geolocation browser API through
 
-
+function startUp() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+  } 
+}
 
 function initializeGeo() {
     geocoder = new google.maps.Geocoder()
+    startUp();
 }
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-} 
-//Get the latitude and the longitude;
+function initialize() {
+  initializeGeo()
+}
+
+// Once the position object is instantiated we can initialize both the
+// gmaps city lookup and the twitter feed API
+
 function successFunction(position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
-    setTimeout(function(){ codeLatLng(lat, lng); }, 250);
+
+// lookup the City with the gmaps City lookup by means of the codeLatLng function
+    codeLatLng(lat, lng);
+// initialize the twitter stream fetch on the basis of lat and long   
+    getData(lat, lng).success(handleData);
 }
 
 function errorFunction(){
     alert("Geocoder failed");
 }
+
+//Gmaps City lookup by means of the geocoder object from gmaps API
+// and the position object from the navigator API
 
 
 function codeLatLng(lat, lng) {
@@ -29,10 +47,6 @@ function codeLatLng(lat, lng) {
     if (status == google.maps.GeocoderStatus.OK) {
       if (results[1]) {
        //formatted address
-
-       // getData(lat, lng)
-      $("#locationFillA").attr("lat", lat)
-      $("#locationFillA").attr("lng", lng)
 
       //find country name
            for (var i=0; i<results[0].address_components.length; i++) {
